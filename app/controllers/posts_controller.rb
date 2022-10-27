34d9cf6c93a66a098190fe08c165e81ea2309474
Robert Neal <robertwneal2@gmail.com>
2022-10-27 15:54:56 -0700

    Devise working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+
+  end
+
+
+  def create
+  
+  end
+
+  def index
+
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root "users#new"
-  devise_for :users
-  resources :users, only: [:new, :create]
+  root "posts#index"
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  # resources :users, only: [:new, :create]
   resources :posts, only: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "users#index"
   
 end
