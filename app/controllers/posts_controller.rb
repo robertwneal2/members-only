86d1f66abd5cc46e868e3d4ba5c356a61f5e1897
Robert Neal <robertwneal2@gmail.com>
2022-10-27 16:58:17 -0700

    Add functionality to create posts. Project complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,27 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def new
-
+    @post = Post.new
   end
 
 
   def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
   
+    if @post.save
+      redirect_to posts_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
+
   end
 
   def index
     @posts = Post.all
   end
-  
+
+  def post_params
+    params.require(:post).permit(:title, :body, :author_id)
+  end
 end
